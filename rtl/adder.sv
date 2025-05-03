2a635976ae763b8b18a287e3f85bca365781f2c5
SinDrag0n <vladislavbelyaev04@gmail.com>
2025-05-03 15:35:01 +0200

    Add (rtl): Добавлены вычислительные блоки с проверкой на переполнение

---
+++ rtl/adder.sv
@@ -0,0 +1,17 @@
+module multiplier #(
+  parameter DATA_WIDTH = 16
+)(
+  input  logic signed [DATA_WIDTH - 1:0]  a_in,
+  input  logic signed [DATA_WIDTH - 1:0]  b_in,
+  output logic signed [DATA_WIDTH - 1:0]  res_o
+);  
+
+
+logic [DATA_WIDTH:0] res;
+assign res   = a_in + b_in;
+
+assign res_o = ( res[DATA_WIDTH] && res[DATA_WIDTH - 1])  ? {1'b0, {DATA_WIDTH - 1{1'b1}}} :
+               ( res[DATA_WIDTH] == res[DATA_WIDTH - 1] ) ? {1'b1, {DATA_WIDTH - 1{1'b0}}} :
+                                                            res[DATA_WIDTH - 1:0];
+
+endmodule
---
+++ rtl/multiplier.sv
@@ -0,0 +1,17 @@
+module multiplier #(
+  parameter DATA_WIDTH = 16
+)(
+  input  logic signed [DATA_WIDTH - 1:0]      a_in,
+  input  logic signed [DATA_WIDTH - 1:0]      b_in,
+  output logic signed [DATA_WIDTH - 1:0]      res_o
+);  
+
+logic [2 * DATA_WIDTH - 1:0] res;
+assign res = a_in * b_in;
+
+assign res_o = ( ~res[2 * DATA_WIDTH - 1] & |res[ 2 * DATA_WIDTH - 2:DATA_WIDTH] ) ? {1'b0, {DATA_WIDTH - 1{1'b1}}} :
+               ( res[2 * DATA_WIDTH - 1] &  &res[ 2 * DATA_WIDTH - 2:DATA_WIDTH] ) ? {1'b1, {DATA_WIDTH - 1{1'b0}}} :
+                                                                                     res[DATA_WIDTH - 1:0];
+
+
+endmodule
